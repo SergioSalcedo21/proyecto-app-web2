b4feb56ceb6e8b8034cd301d0972364708e93a7c
SergioSalcedo21 <sa.salcedo2115@gmail.com>
2026-08-20 20:32:58 -0600

    Update Seguridad...

--- cafeteriasergio/settings.py
+++ cafeteriasergio/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     #Middleware personalizado para auditoría de seguridad
+    'pedidos.middleware.AuditoriaSeguridadMiddleware',
 ]
 
 ROOT_URLCONF = 'cafeteriasergio.urls'
@@ -123,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+#configuraciones de los archivos multimedia de media file
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
